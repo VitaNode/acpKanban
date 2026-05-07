9fac7eb6a9e6c602f45a9cc9035a260eb5eaf320
lanzf <lanzf@lanzfMBA.local>
2026-05-07 13:54:27 +0800

    fix: 添加 services.dart 导入修复编译错误
    - 添加 import 'package:flutter/services.dart' 导入
    - 修复 KeyDownEvent、LogicalKeyboardKey、HardwareKeyboard 未定义的错误

--- flutter_prototype/lib/screens/card_detail_screen.dart
+++ flutter_prototype/lib/screens/card_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../models/kanban_card.dart';
 import '../models/kanban_column.dart';
 import '../models/card_message.dart';
